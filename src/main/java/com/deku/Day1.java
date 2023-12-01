08e54837d9333f99373fa78c161bd621abfd1455
Nick Moresco <nick.moresco@workday.com>
2023-12-01 08:24:59 -0800

    make utility to read lines of input

--- src/main/java/com/deku/Day1.java
+++ src/main/java/com/deku/Day1.java
@@ -1,11 +1,18 @@
 package com.deku;
 
+import static com.deku.Util.printLines;
+import static com.deku.Util.readLines;
+
+import java.util.List;
+
 public class Day1 implements Puzzle {
     @Override public void part1() {
-
+        final List<String> strings = readLines("day1.txt");
+        printLines(strings);
     }
 
     @Override public void part2() {
 
     }
+
 }
---
+++ src/main/java/com/deku/Util.java
@@ -0,0 +1,46 @@
+package com.deku;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Util {
+
+    public static List<String> readLines(final String file) {
+        try {
+            URL url = Util.class.getResource('/' + file);
+            if (url == null) {
+                throw new FileNotFoundException();
+            }
+
+            try (var reader = new FileReader(url.getPath())) {
+                var br = new BufferedReader(reader);
+
+                var lines = new ArrayList<String>();
+                String line;
+                while ((line = br.readLine()) != null) {
+                    lines.add(line);
+                }
+
+                return lines;
+            }
+        }
+        catch (FileNotFoundException e) {
+            System.err.println("Could not find file " + file);
+        }
+        catch (IOException e) {
+            System.err.println("Could not parse file " + file);
+        }
+
+        return Collections.emptyList();
+    }
+
+    public static void printLines(List<String> lines) {
+        lines.forEach(System.out::println);
+    }
+}
